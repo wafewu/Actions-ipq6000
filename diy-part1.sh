a68151913655dfd80207903edb07a0274be12f20
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-01-18 10:07:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,18 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# sed -i '1,2d' feeds.conf.default
+# sed -i '$a src-git packages https://github.com/KK2018gh/ipq6000_packages.git;1806' feeds.conf.default
+# sed -i '$a src-git luci https://github.com/KK2018gh/ipq6000_luci.git;1806' feeds.conf.default
+
+# Add luci-theme-argon
+# rm -rf lede/package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git
+# rm -rf package/lean/luci-theme-argon/
+# git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# 添加额外软件包
+git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
