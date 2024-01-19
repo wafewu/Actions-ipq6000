e3fc508faffd331b673b8a4c303cd7bf09a46d4b
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-01-19 12:29:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,7 +27,3 @@
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by 风间琉璃/g" package/lean/default-settings/files/zzz-default-settings
