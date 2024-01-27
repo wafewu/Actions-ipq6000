474a3aea87b99b442b41bc83666b3adae8318c9d
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-01-27 21:07:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Add a feed source
-echo sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add luci-theme-argon
 # rm -rf lede/package/lean/luci-theme-argon
