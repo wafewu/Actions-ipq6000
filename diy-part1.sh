4b2bf72b25dadeee48e26a9d6b35260f9cec5c35
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-02-01 13:15:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,9 @@ git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/l
 
 # 添加额外软件包
 # Adguardhome
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+#git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+#git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 # 科学上网
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
