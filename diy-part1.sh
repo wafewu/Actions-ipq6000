65368a3dafaf0e8983d2b8b7d4467f27c6dc296a
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-02-02 23:46:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
-
-
-
-
-
 
 # 添加额外软件包
 # Adguardhome
