b17f324bd663cdfa110f623fa8227b11f229e1a8
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-02-13 10:53:28 +0800

    Update diy-part1.sh

--- scripts/mt798x/diy-part1.sh
+++ scripts/mt798x/diy-part1.sh
@@ -1,4 +1,4 @@
 
 # 添加额外软件包
 # Adguardhome
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+#git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
