617895e115613defe02f6e9075c006afc993b44e
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-02-18 21:00:01 +0800

    Update diy-part1.sh

--- scripts/ipq6000/diy-part1.sh
+++ scripts/ipq6000/diy-part1.sh
@@ -13,7 +13,6 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 #rm -rf feeds/packages/net/curl
 #git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
-sudo apt install libopenssl-legacy
 
 # 添加额外软件包
 # Adguardhome
