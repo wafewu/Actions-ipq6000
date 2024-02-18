db3ab73cb073a15cfe5c9a956bf7c72182b4d4e6
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-02-18 16:42:16 +0800

    Update diy-part1.sh

--- scripts/ipq6000/diy-part1.sh
+++ scripts/ipq6000/diy-part1.sh
@@ -11,7 +11,8 @@
 #
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
-
+rm -rf feeds/packages/net/curl
+git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
 
 
 # 添加额外软件包
