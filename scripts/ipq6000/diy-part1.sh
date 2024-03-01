a9c7ab0dfe1cb5483d055ae66c355f03d7e5c367
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-03-02 04:26:20 +0800

    Update diy-part1.sh

--- scripts/ipq6000/diy-part1.sh
+++ scripts/ipq6000/diy-part1.sh
@@ -10,8 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
 
 
 
