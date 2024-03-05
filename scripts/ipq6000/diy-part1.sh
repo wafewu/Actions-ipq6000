4d843abac0514b62b3e4b7488f993d5d5bc46bc5
lfsakura0 <135017778+lfsakura0@users.noreply.github.com>
2024-03-05 10:39:09 +0800

    Update diy-part1.sh

--- scripts/ipq6000/diy-part1.sh
+++ scripts/ipq6000/diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 
-rm -rf feeds/luci/applications/luci-app-openclash
+
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 
@@ -35,7 +35,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
-git clone master https://github.com/vernesong/OpenClash luci-app-openclash
+
 
 # 添加额外软件包
 # Adguardhome
